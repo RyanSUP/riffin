46be23ffe89b4d7f6a656722a460d7a376854a20
RMo <morici23@gmail.com>
2022-03-07 21:08:34 -0500

    user can delete collections.

--- controllers/collections.js
+++ controllers/collections.js
@@ -4,7 +4,6 @@ const index = (req, res) => {
     Collection.find({owner: req.user.profile._id})
     .populate('tabs')
     .then((collections) => {
-        console.log('colect', collections)
         res.render('collections/index', {
             title: 'My collections',
             collections,
@@ -25,7 +24,41 @@ const create = (req, res) => {
     })
 }
 
+const show = (req, res) => {
+    Collection.findById(req.params.id)
+    .populate('tabs')
+    .then(collection => {
+        res.render('collections/show', {
+            collection,
+            tabs: collection.tabs,
+            title: `${collection.name} licks`
+        })
+    })
+    .catch(error => {
+        console.log(error)
+        res.redirect('/collections')
+    })
+}
+
+const deleteCollection = (req, res) => {
+    Collection.findById(req.params.id)
+    .then(collection => {
+        if(collection.owner.equals(req.user.profile._id)) {
+            collection.delete()
+            .then(()=> {
+                res.redirect('/collections')
+            })
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.redirect('/collections')
+    })
+}
+
 export {
     index,
     create,
+    show,
+    deleteCollection as delete,
 }
--- routes/collections.js
+++ routes/collections.js
@@ -8,6 +8,12 @@ const router = Router()
 // GET - localhost:3000/collections
 router.get('/', isLoggedIn, collectionsCtrl.index)
 
+// GET - localhost:3000/collections/:id
+router.get('/:id', isLoggedIn, collectionsCtrl.show)
+
+// POST - localhost:3000/collections/:id
+router.delete('/:id', collectionsCtrl.delete)
+
 export {
     router
 }
